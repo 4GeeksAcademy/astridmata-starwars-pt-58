c7392392ea2ed8ec38e486bb1e58746666a84a1b
Astrid Mata <86080960+AstridMata@users.noreply.github.com>
2025-11-08 02:10:50 +0000

    starward people

---
+++ src/components/CardPeople.jsx
@@ -0,0 +1,25 @@
+
+import useGlobalReducer from "../hooks/useGlobalReducer";
+import { Link } from "react-router-dom";
+
+
+const CardPeople = ({item}) => {
+
+    return (
+        <div>
+            <div className="card" style={{ width: "18rem" }}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    {/* <h5 className="card-title">Name: {item.name}</h5> */}
+                    <p className="card-text">k of the card’s content.</p>
+                    <Link  className="btn btn-primary">Go somewhere</Link>
+                </div>
+            </div>
+        </div>
+    )
+
+
+
+}
+
+export default CardPeople
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,31 @@
+import { useEffect } from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-
+import CardPeople from "../components/CardPeople.jsx";
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
+
+	function character() {
+		fetch("https://www.swapi.tech/api/people/")
+			.then(res => res.json())
+			.then(data => console.log(data.results))
+			.catch(err => console.error(err))
+	}
+
+
+
+	useEffect(()=>{
+		character()
+	},[])
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+			<h1>Hello Starwars!</h1>
+			
+
+			<CardPeople/>
+
 		</div>
 	);
 }; 
